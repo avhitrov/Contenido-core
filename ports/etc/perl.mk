a286768a538a01d662efad0bf58a37d96666e19c
ahitrov <ahitrov@c359e86c-732b-df11-abe2-00110913f5fd>
2013-09-29 18:58:43 +0000

    Major version dir - fast patch for Perl 5.12.5
    git-svn-id: http://contenido.me/repos/Contenido/utf8/core@382 c359e86c-732b-df11-abe2-00110913f5fd

--- ports/etc/perl.mk
+++ ports/etc/perl.mk
@@ -10,6 +10,7 @@ ifeq (${shell ${PERL} -e '${PERL_LEVEL}<500600 && print 1'},1)
 PERL_VER ?=			${shell ${PERL} -e '$$]=~/(\d+)\.(\d{3})(\d+)/; printf "%d.%03d", $$1,$$2;'}
 else
 PERL_VER ?=			${shell ${PERL} -e '$$]=~/(\d+)\.(\d{3})(\d+)/; printf "%d.%d.%d", $$1,$$2,$$3;'}
+PERL_MAJOR_VER ?=		${shell ${PERL} -e '$$]=~/(\d+)\.(\d{3})(\d+)/; printf "%d.%d", $$1,$$2;'}
 endif
 
 ifeq (${shell ${PERL} -e '${PERL_LEVEL}<500600 && print 1'},1)
@@ -21,6 +22,8 @@ endif
 PERL_LIB ?=			${PREFIX}/lib/perl5/${PERL_VER}					\
 				${PREFIX}/lib/perl5/${PERL_VER}/${PERL_ARCH}			\
 				${PREFIX}/lib/perl5/site_perl					\
+				${PREFIX}/lib/perl5/site_perl/${PERL_MAJOR_VER}			\
+				${PREFIX}/lib/perl5/site_perl/${PERL_MAJOR_VER}/${PERL_ARCH}	\
 				${PREFIX}/lib/perl5/site_perl/${PERL_VER}			\
 				${PREFIX}/lib/perl5/site_perl/${PERL_VER}/${PERL_ARCH}
 PERL_LIBCOL ?=			${shell echo ${PERL_LIB} | perl -pe 's/\s+$$//; s/\s+/:/g'}
